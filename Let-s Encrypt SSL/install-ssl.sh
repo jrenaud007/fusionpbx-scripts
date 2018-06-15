5f8bdd81daa3bda4e75cf8bbdafe60f3a335302c
Jonathan R <31027727+jrenaud007@users.noreply.github.com>
2018-06-15 18:17:41 -0400

    Update install-ssl.sh

--- Let-s Encrypt SSL/install-ssl.sh
+++ Let-s Encrypt SSL/install-ssl.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+#Let's encrypt binaries location 
 letsencryptbin=/opt/letsencrypt/
+
+echo Please enter you domain name
+read $domainname
+
+read -p "is $domainname correct ? (y/n)?" choice
+case "$choice" in 
+  y|Y ) echo "yes";;
+  n|N ) echo "no";;
+  * ) echo "invalid";;
+esac
 
 if [ -d "$letsencryptbin" ]; then
   echo "Let's encrypt is already installed on your system, please run the script with -add argument"
@@ -8,3 +19,10 @@ if [ -d "$letsencryptbin" ]; then
 fi
 
 git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
+
+cd /opt/letsencrypt
+chmod a+x /opt/letsencrypt/certbot-auto
+./certbot-auto
+cd /etc/letsencrypt/
+mkdir -p configs
+cd configs
